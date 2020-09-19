290c43ea37587b6010620326e591c7ad1b424055
burcak <burcakose@gmail.com>
2020-09-19 23:47:55 +0200

    added rocket animations

--- rocket.js
+++ rocket.js
@@ -1,7 +1,7 @@
 var timer = null;
 var countDownNumber = 10;
 
-var changeState = function(state) {
+var changeState = function (state) {
 	document.body.className = 'body-state' + state;
 
 	clearInterval(timer);
@@ -16,6 +16,20 @@ var changeState = function(state) {
 			if(countDownNumber <= 0) {
 				changeState(3);
 			}
+
+			if (countDownNumber > 1 && countDownNumber <= 4) {
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
+			if (countDownNumber > 4 && countDownNumber <= 7) {
+				document.getElementById('nervous').className = 'nervous show';
+			} else {
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			
 		}, 500);
 	} else if (state == 3) {
 		var success = setTimeout(function () {
@@ -31,3 +45,4 @@ var changeState = function(state) {
 		}, 2000);
 	}
 }
+
